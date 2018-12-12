59bbfeab48d1bb935f6a3a3e280ed09a23912fe7
Masahiro Kitazawa <masahiro.kitazawa@gmail.com>
2018-12-13 08:02:22 +0900

    WIP: add disseminator

---
+++ extern/inet/src/inet/applications/udpapp/UDPCamListener.cc
@@ -0,0 +1,90 @@
+//
+// Copyright (C) 2011 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/applications/udpapp/UDPCamListener.h"
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/transportlayer/contract/udp/UDPControlInfo_m.h"
+
+namespace inet {
+
+Define_Module(UDPCamListener);
+
+simsignal_t UDPCamListener::rcvdPkSignal = registerSignal("pk");
+
+void UDPCamListener::initialize(int stage)
+{
+    std::cout << "helleeeeeeeeee" << endl;
+    std::cout << stage << endl;
+    ApplicationBase::initialize(stage);
+
+    if (stage == INITSTAGE_LOCAL) {
+        // init statistics
+    }
+}
+
+void UDPCamListener::handleMessageWhenUp(cMessage *msg)
+{
+    std::cout << "hello" << endl;
+    if (msg->getKind() == UDP_I_DATA) {
+        receiveCAM(PK(msg));
+    }
+    else {
+        throw cRuntimeError("Message received with unexpected message kind = %d", msg->getKind());
+    }
+    std::cout << "aaa" << endl;
+}
+
+void UDPCamListener::receiveCAM(cPacket *pk)
+{
+    EV_INFO << "Video stream packet: " << UDPSocket::getReceivedPacketInfo(pk) << endl;
+    emit(rcvdPkSignal, pk);
+
+    // determine its source address/port
+    UDPDataIndication *ctrl = check_and_cast<UDPDataIndication *>(pk->getControlInfo());
+    L3Address srcAddress = ctrl->getSrcAddr();
+    std::cout << "cam received" << UDPSocket::getReceivedPacketInfo(pk) << " from:" << srcAddress << endl;
+//    this->getParentModule()->getParentModule()
+    delete pk;
+}
+
+
+void UDPCamListener::finish()
+{
+    ApplicationBase::finish();
+}
+
+bool UDPCamListener::handleNodeStart(IDoneCallback *doneCallback)
+{
+    int localPort = par("localPort");
+    socket.setOutputGate(gate("udpOut"));
+    socket.bind(localPort);
+    return true;
+}
+
+bool UDPCamListener::handleNodeShutdown(IDoneCallback *doneCallback)
+{
+    //TODO if(socket.isOpened()) socket.close();
+    return true;
+}
+
+void UDPCamListener::handleNodeCrash()
+{
+}
+
+} // namespace inet
+
--- extern/inet/src/inet/applications/udpapp/UDPCamListener.h
+++ extern/inet/src/inet/applications/udpapp/UDPCamListener.h
@@ -1,20 +1,22 @@
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
+// Copyright (C) 2011 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
 
-#ifndef __INET_UDPCAMAPP_H_
-#define __INET_UDPCAMAPP_H_
+#ifndef __INET_UDPCAMLISTENER_H
+#define __INET_UDPCAMLISTENER_H
 
 #include "inet/common/INETDefs.h"
 
@@ -24,37 +26,28 @@
 namespace inet {
 
 /**
- * TODO - Generated class
+ * UDP application. See NED for more info.
  */
-class INET_API UDPCAMApp : public ApplicationBase
+class INET_API UDPCamListener : public ApplicationBase
 {
   protected:
-
-    //state
     UDPSocket socket;
-    cMessage *selfMsg = nullptr;
-
-    //statistics
     static simsignal_t rcvdPkSignal;
 
   protected:
     virtual int numInitStages() const override { return NUM_INIT_STAGES; }
     virtual void initialize(int stage) override;
-    virtual void finish() override;
     virtual void handleMessageWhenUp(cMessage *msg) override;
+    virtual void finish() override;
 
-    virtual void receiveCam(cPacket *msg);
+    virtual void receiveCAM(cPacket *msg);
 
-    // ApplicationBase:
     virtual bool handleNodeStart(IDoneCallback *doneCallback) override;
     virtual bool handleNodeShutdown(IDoneCallback *doneCallback) override;
     virtual void handleNodeCrash() override;
-
-  public:
-    UDPCAMApp() { }
-    virtual ~UDPCAMApp() { cancelAndDelete(selfMsg); }
 };
 
-} //namespace
+} // namespace inet
 
-#endif
+#endif // ifndef __INET_UDPCAMLISTENER_H
+
