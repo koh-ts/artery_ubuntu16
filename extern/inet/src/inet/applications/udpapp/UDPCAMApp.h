45f9b7e810c660cbccdfa2be7982e39cdfd5988f
Masahiro Kitazawa <masahiro.kitazawa@gmail.com>
2018-12-13 02:07:22 +0900

    WIP: add udpcamapp

---
+++ extern/inet/src/inet/applications/udpapp/UDPCAMApp.h
@@ -0,0 +1,60 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __INET_UDPCAMAPP_H_
+#define __INET_UDPCAMAPP_H_
+
+#include "inet/common/INETDefs.h"
+
+#include "inet/applications/base/ApplicationBase.h"
+#include "inet/transportlayer/contract/udp/UDPSocket.h"
+
+namespace inet {
+
+/**
+ * TODO - Generated class
+ */
+class INET_API UDPCAMApp : public ApplicationBase
+{
+  protected:
+
+    //state
+    UDPSocket socket;
+    cMessage *selfMsg = nullptr;
+
+    //statistics
+    static simsignal_t rcvdPkSignal;
+
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void finish() override;
+    virtual void handleMessageWhenUp(cMessage *msg) override;
+
+    virtual void receiveCam(cPacket *msg);
+
+    // ApplicationBase:
+    virtual bool handleNodeStart(IDoneCallback *doneCallback) override;
+    virtual bool handleNodeShutdown(IDoneCallback *doneCallback) override;
+    virtual void handleNodeCrash() override;
+
+  public:
+    UDPCAMApp() { }
+    virtual ~UDPCAMApp() { cancelAndDelete(selfMsg); }
+};
+
+} //namespace
+
+#endif
