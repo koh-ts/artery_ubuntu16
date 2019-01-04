9bde9860e1aa3ec8e6fbcd6dc01d9f27658309d3
Masahiro Kitazawa <masahiro.kitazawa@gmail.com>
2019-01-05 00:38:14 +0900

    patch to analysis.py

--- split.py
+++ split.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-methods = ["passive_grid_pcam"] #, "naive_grid_pcam", "passive_grid_pcam"]
+methods = ["passive_grid_pcam", "naive_grid_pcam", "passive_grid_pcam"]
 pcam_nums = ["num_5", "num_10", "num_15"]
 
 simStartTime = 200
@@ -8,7 +8,7 @@ simTotalInterval = 15
 
 for method in methods:
     for pcam_num in pcam_nums:
-        outputDir = Path("output/" + method + "/" + pcam_num + "/sim_all/")
+        outputDir = Path("output/" + method + "/" + pcam_num + "/sim_all_1/")
         files = outputDir.glob("*")
         for file in files:
             print(file)
@@ -26,6 +26,7 @@ for method in methods:
                         f.close()
             else:
                 count = 0
+                # count = 47
                 startPos = 0
                 for i,line in enumerate(lines):
                     objs = line.split("\t")
@@ -33,6 +34,9 @@ for method in methods:
                         of_path = str(outputDir.parent) + "/sim_" + str(count) + "/" + file.name
                         of = open(of_path,mode="w")
                         for oline in lines[startPos:i]:
+                            # objs = oline.split("\t")
+                            # objs[0] = objs[0].split(":")[0] + ": " + str(float(objs[0].split(":")[1]) + 705)
+                            # oline = "\t".join(objs)
                             of.write(oline)
                         startPos = i
                         count += 1
