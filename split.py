c4d221d1f56fe21a2e0c08d2d74cb719917d2803
Masahiro Kitazawa <masahiro.kitazawa@gmail.com>
2019-01-06 15:52:21 +0900

    fix: split.py

--- split.py
+++ split.py
@@ -1,14 +1,17 @@
 from pathlib import Path
 
-methods = ["passive_grid_pcam", "naive_grid_pcam", "passive_grid_pcam"]
+methods = ["original_pcam", "naive_grid_pcam", "passive_grid_pcam"]
 pcam_nums = ["num_5", "num_10", "num_15"]
 
 simStartTime = 200
 simTotalInterval = 15
 
+offset = 94
+
+
 for method in methods:
     for pcam_num in pcam_nums:
-        outputDir = Path("output/" + method + "/" + pcam_num + "/sim_all_2/")
+        outputDir = Path("output/" + method + "/" + pcam_num + "/sim_all/")
         files = outputDir.glob("*")
         for file in files:
             print(file)
@@ -25,22 +28,28 @@ for method in methods:
                         f = open(str(path) + "/" + file.name, mode="w")
                         f.close()
             else:
-                # count = 0
-                count = 47
+                count = 0
                 startPos = 0
                 for i,line in enumerate(lines):
                     objs = line.split("\t")
                     if float(objs[0].split(":")[1]) >= simStartTime + simTotalInterval * (count + 1):
-                        of_path = str(outputDir.parent) + "/sim_" + str(count) + "/" + file.name
+                        of_path = str(outputDir.parent) + "/sim_" + str(count + offset) + "/" + file.name
                         of = open(of_path,mode="w")
                         for oline in lines[startPos:i]:
                             objs = oline.split("\t")
-                            objs[0] = objs[0].split(":")[0] + ": " + str(float(objs[0].split(":")[1]) + 705)
+                            objs[0] = objs[0].split(":")[0] + ": " + str(float(objs[0].split(":")[1]) + offset * simTotalInterval)
+                            if "rsu" in str(file):
+                                objs[2] = objs[2].split(":")[0] + ": " + str(float(objs[2].split(":")[1])+ offset * simTotalInterval * pow(10,6))
                             oline = "\t".join(objs)
                             of.write(oline)
                         startPos = i
                         count += 1
-                of_path = str(outputDir.parent) + "/sim_" + str(count) + "/" + file.name
+                of_path = str(outputDir.parent) + "/sim_" + str(count + offset) + "/" + file.name
                 of = open(of_path,mode="w")
                 for oline in lines[startPos:i]:
+                    objs = oline.split("\t")
+                    objs[0] = objs[0].split(":")[0] + ": " + str(float(objs[0].split(":")[1]) + offset * simTotalInterval)
+                    if "rsu" in str(file):
+                        objs[2] = objs[2].split(":")[0] + ": " + str(float(objs[2].split(":")[1]) + offset * simTotalInterval * pow(10,6))
+                    oline = "\t".join(objs)
                     of.write(oline)
