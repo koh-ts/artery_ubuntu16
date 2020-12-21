5cf2dacd0cdc15ac8ab938875040d0d48f6184e1
koh-ts <k0702611@gmail.com>
2020-12-21 23:03:19 +0900

    WIP rsu

--- scenarios/highway-police/PoliceServiceEnvmod.cc
+++ scenarios/highway-police/PoliceServiceEnvmod.cc
@@ -19,9 +19,9 @@ void PoliceServiceEnvmod::trigger()
     const auto& objects = localEnvmod->allObjects();
     if (objects.size() > 0 || skippedTrigger >= 4) {
         PoliceService::trigger();
-        for(int i = 0; i < objects.size(); ++i){
-            std::cout << typeid(objects).name() << std::endl;
-        }
+        // for(int i = 0; i < objects.size(); ++i){
+        //     std::cout << typeid(objects).name() << std::endl;
+        // }
         skippedTrigger = 0;
     } else {
         std::cout << "skippedTrigger = " << skippedTrigger << std::endl;
